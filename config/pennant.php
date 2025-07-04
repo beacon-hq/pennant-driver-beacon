46b843f975ec9a57e5dcd1c3462b895c05f2657c
Davey Shafik <me@daveyshafik.com>
2025-07-04 05:58:16 -0700

    feat: Allow setting the environment via config

--- config/pennant.php
+++ config/pennant.php
@@ -7,10 +7,11 @@ return [
         'beacon' => [
             'driver' => 'beacon',
             'app_name' => env('BEACON_APP_NAME', env('APP_NAME', 'Laravel')),
+            'environment' => env('BEACON_ENVIRONMENT', env('APP_ENV', 'local')),
             'url' => env('BEACON_API_URL', 'http://localhost/'),
             'path_prefix' => env('BEACON_API_PATH_PREFIX', '/api'),
             'cache_ttl' => 1800,
-            'api_key' => env('BEACON_API_TOKEN', 'secret'),
+            'api_key' => env('BEACON_ACCESS_TOKEN', 'secret'),
         ],
     ],
 ];
